55a964655edb4a8afedd436d0b9cf056b5a13674
ChenWils <wilson20020215second@gmail.com>
2024-12-03 15:19:38 +0800

    b1

--- hw4.sh
+++ hw4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # python3 .py $1 $2
-python ./guassian-splattong/my_guassian-splatting/render.py -m ./output -s $1 $2
+python ./guassian-splattong/my_guassian-splatting/render.py -m ./output -s $1 --op_path$2
 # TODO - run your inference Python3 code
